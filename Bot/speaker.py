2451304f7f528eab036d71c1ec0818879f2cd256
LopezS14 <lopezsdeyanira@gmail.com>
2024-09-07 06:20:06 -0600

    Update speaker.py

--- Bot/speaker.py
+++ Bot/speaker.py
@@ -1,102 +1,97 @@
 import streamlit as st
 from gtts import gTTS
-from playsound import playsound
-import tempfile
-import os
+import pygame
 from PIL import Image
+import os
+import tempfile
 from brain import predict_class, get_response, intents
 
 # Configuración de la página
 st.set_page_config(
-    page_title="UPIIH BOT",  # Título en la ventana del navegador
-    page_icon="https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M3.gif",  # Icono de la página
-    layout="wide",  # Opcional: "wide" o "centered"
-    initial_sidebar_state="expanded"  # Opcional: "expanded" o "collapsed"
+    page_title="UPIIH BOT",
+    page_icon="https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M3.gif",
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
 
 # Sidebar
-st.sidebar.markdown("<h1 style='font-size: 20px;'>Genera tu planeación didáctica con ayuda de un chatbot</h1>", unsafe_allow_html=True)
+st.sidebar.markdown("<h1 style='font-size: 20px;'>Genera tu planeacion didactica con ayuda de un chatbot</h1>", unsafe_allow_html=True)
 
 # Header
-image_path = "https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M3.gif"
-image = Image.open(image_path)
 image_url = 'https://img.freepik.com/foto-gratis/acuarela-color-rojo-oscuro-textura-fondo-pintado-mano-fondo-color-vino-tinto-acuarela_145343-192.jpg?size=626&ext=jpg'
 st.markdown("""
     <style>
-    /* Estilos para el contenedor del encabezado */
     .header-container {
         display: flex;
-        flex-direction: column; /* Cambia la dirección a columna para que los subtemas estén debajo del título */
+        flex-direction: column;
         align-items: center;
         padding: 10px;
         background-color: #ffff;
     }
     
-    /* Estilos para los logos */
     .header-logo {
-        width: 120px; /* Tamaño de imagen aumentado */
-        height: 80px; /* Tamaño de imagen aumentado */
-        margin: 0 10px; /* Espacio alrededor de las imágenes */
+        width: 120px;
+        height: 80px;
+        margin: 0 10px;
     }
     
-    /* Estilos para el texto del encabezado */
     .header-title {
         color: black;
-        font-size: 1.5em; /* Tamaño de fuente que se ajusta automáticamente */
-        margin: 0; /* Elimina el margen predeterminado */
-        text-align: center;
+        font-size: 1.5em;
+        margin:0;
+        text-align:center;
     }
     
-    /* Estilos para los subtemas */
     .subtopics {
         text-align: center;
-        font-size: 1.5em; /* Tamaño de fuente para pantallas grandes */
-        margin: 0; /* Elimina el margen predeterminado */
-        color: black;
+        font-size: 1.5em;
+        margin: 0;
+        color:black;
     }
     
     .subtopics p {
-        margin: 0; /* Espacio reducido entre párrafos */
+        margin:0;
     }
 
-    /* Media queries para hacer el diseño responsivo */
     @media (max-width: 600px) {
         .header-title {
             font-size: 0.9em;
         }
         .subtopics {
-            font-size: 1em; /* Tamaño de fuente más pequeño en pantallas más pequeñas */
+            font-size: 1em;
         }
         .header-logo {
-            width: 60px; /* Tamaño de imagen reducido para pantallas pequeñas */
-            height: 60px; /* Tamaño de imagen reducido para pantallas pequeñas */
+            width:60px;
+            height: 60px;
         }
+        
     }
     .circle-img {
         border-radius: 50%;
-        width: 100px; /* Ajusta el tamaño según lo necesario */
-        height: 100px; /* Ajusta el tamaño según lo necesario */
+        width: 100px;
+        height: 100px;
         object-fit: cover;
         display: block;
         margin-left: auto;
         margin-right: auto;
     }
+    
     </style>
-
     <div class="header-container">
         <div style="display: flex; justify-content: space-between; width: 100%; align-items: center;">
             <img src="https://th.bing.com/th/id/OIP.MQI9waMb4IGJ52U8KF5gmgHaHa?rs=1&pid=ImgDetMain" class="header-logo">
-            <h3 class="header-title">Instituto Politécnico Nacional</h3>
+            <h3 class="header-title">Instituto Politecnico Nacional</h3>
             <img src="https://th.bing.com/th/id/R.8119ac7aaccd85c2837ae19087717f56?rik=3mREtV8uaKpCGg&pid=ImgRaw&r=0" class="header-logo">
         </div>
         <div class="subtopics">
             <p>Secretaría Académica</p>
             <p>Dirección de Educación Superior</p>
-            <p>Unidad Profesional Interdisciplinaria Campus de Ingeniería Hidalgo</p>
+            <p>Unidad Profesional Interdisciplinaria Campus de Ingenieria Hidalgo</p>
         </div>
     </div>
     """, unsafe_allow_html=True)
 
+# Fondo de la app
 st.markdown(f"""
     <style>
     .stApp {{
@@ -108,62 +103,62 @@ st.markdown(f"""
     </style>
     """, unsafe_allow_html=True)
 
-# Sidebar image
-with st.sidebar:
-    st.image(image, use_column_width=True)
+# Cargar imagen en el sidebar
+user_avatar = "https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M3.gif"
+st.sidebar.image(user_avatar, use_column_width=True)
 
+# Función para hablar
 def speak(text):
     temp_audio_file = None
     try:
-        # Crear archivo temporal
         temp_audio_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
         tts = gTTS(text=text, lang='es')
         tts.save(temp_audio_file.name)
         temp_audio_file_path = temp_audio_file.name
         temp_audio_file.close()
 
-        # Reproducir audio
-        playsound(temp_audio_file_path)
+        pygame.mixer.init()
+        pygame.mixer.music.load(temp_audio_file_path)
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
     finally:
         if temp_audio_file:
             try:
                 os.unlink(temp_audio_file_path)
             except PermissionError:
-                pass  # Manejo de excepción si el archivo aún está en uso
+                pass
 
-# Inicialización de estado de la sesión
+# Lógica del chatbot
 if "messages" not in st.session_state:
     st.session_state.messages = []
 if "first_message" not in st.session_state:
     st.session_state.first_message = True
 if "user_avatar" not in st.session_state:
-    st.session_state.user_avatar = "user.png"  # Ruta del avatar del usuario
+    st.session_state.user_avatar = "user.png"
 
-# Mostrar mensajes
 for message in st.session_state.messages:
-    with st.chat_message(message["role"], avatar="https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M3.gif" if message["role"] == "Bot" else st.session_state.user_avatar):
+    with st.chat_message(message["role"], avatar=user_avatar if message["role"] == "Bot" else st.session_state.user_avatar):
         st.markdown(message["content"])
 
-# Mensaje inicial
 if st.session_state.first_message:
     initial_message = "Hola, ¿cómo puedo ayudarte?"
-    with st.chat_message("Bot", avatar="https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M3.gif"):
+    with st.chat_message("Bot", avatar=user_avatar):
         st.markdown(initial_message)
     st.session_state.messages.append({"role": "Bot", "content": initial_message})
     st.session_state.first_message = False
-    speak(initial_message)  # Hablar el mensaje inicial
+    speak(initial_message)
 
-# Entrada del usuario
 if prompt := st.chat_input("¿Cómo puedo ayudarte?"):
     with st.chat_message("user", avatar=st.session_state.user_avatar):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Implementación del algoritmo de la IA
     insts = predict_class(prompt)
     res = get_response(insts, intents)
 
-    with st.chat_message("Bot", avatar="https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M3.gif"):
+    with st.chat_message("Bot", avatar=user_avatar):
         st.markdown(res)
     st.session_state.messages.append({"role": "Bot", "content": res})
-    speak(res)  # Hablar la respuesta
+    speak(res)
