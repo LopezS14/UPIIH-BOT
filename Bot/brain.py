e9e8407f9477d9b6e610d65df2b8e7464f81ff45
LopezS14 <lopezsdeyanira@gmail.com>
2024-09-07 11:53:11 -0600

    Update brain.py

--- Bot/brain.py
+++ Bot/brain.py
@@ -63,9 +63,9 @@ def predict_class(sentence):
 # Diccionario de rutas de documentos
 doc_paths = {
     "Sistemas automotrices semestre 7": "Bot/automotricesSemestre7.docx",
-    "Ingenieria mecatronica semestre 1": "mecatronica1.docx",
+    "Ingenieria mecatronica semestre 1": "",
     "Sistemas automotrices semestre 7-programasintetico": "https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/SA_PS7.pdf",
-    "Ingenieria mecatronica semestre 1-programasintetico": "M_PS1.pdf"
+    "Ingenieria mecatronica semestre 1-programasintetico": "https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/M_PS1.pdf"
 }
 
 
