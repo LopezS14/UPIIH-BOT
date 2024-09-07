dcbffbe37f34c7729ee4c76309158bcdba25a290
LopezS14 <lopezsdeyanira@gmail.com>
2024-09-07 11:29:15 -0600

    Update brain.py

--- Bot/brain.py
+++ Bot/brain.py
@@ -62,7 +62,7 @@ def predict_class(sentence):
 
 # Diccionario de rutas de documentos
 doc_paths = {
-    "Sistemas automotrices semestre 7": "https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/automotricesSemestre7.docx",
+    "Sistemas automotrices semestre 7": "https://github.com/LopezS14/UPIIH-BOT/blob/main/Bot/automotricesSemestre7.docx,
     "Ingenieria mecatronica semestre 1": "mecatronica1.docx",
     "Sistemas automotrices semestre 7-programasintetico": "SA_PS7.pdf",
     "Ingenieria mecatronica semestre 1-programasintetico": "M_PS1.pdf"
