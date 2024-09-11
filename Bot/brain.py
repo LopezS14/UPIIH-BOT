843c48b8bdead9a162861768cd7c0ec0af67c899
LopezS14 <lopezsdeyanira@gmail.com>
2024-09-10 19:34:18 -0600

    Update brain.py

--- Bot/brain.py
+++ Bot/brain.py
@@ -64,7 +64,8 @@ def predict_class(sentence):
 doc_paths = {
     "Sistemas automotrices semestre 7": "Bot/automotricesSemestre7.docx",
     "Sistemas automotrices semestre 7-programasintetico": "Bot/SA_PS7.pdf",
-    "Ingenieria mecatronica semestre 1-programasinetico": "Bot/M_PS1.pdf"
+    "Ingenieria mecatronica semestre 1-programasinetico": "Bot/M_PS1.pdf",
+    "Ingenieria mecatronica semestre 1": "mecatronica 1.docx"
 }
 
 # Función para manejar el documento y proporcionar el botón de descarga
