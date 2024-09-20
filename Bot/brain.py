50385ec26b1aea428b128b41c34618a6ee014298
Deyanira Lopez <lopezsdeyanira@gmail.com>
2024-09-20 09:57:40 -0600

    update paths

--- Bot/brain.py
+++ Bot/brain.py
@@ -65,7 +65,7 @@ doc_paths = {
     "Sistemas automotrices semestre 7-programasintetico": "Bot/SA_PS7.pdf",
     "Ingenieria mecatronica semestre 1-programasintetico": "Bot/M_PS1.pdf",
     "Ingenieria mecatronica semestre 1": "Bot/ingenieriaMecatronica_1.docx",
-    "Sistemas automotrices semestre 7": "Bot/automotricesSemestre7.docx"
+    "1": "Bot/automotricesSemestre7.docx"
     }
 
 
